a22e42dfa29850fa26cea6936ddfd9b8a64f3814
orkundemirtas <demirtasorkun9@gmail.com>
2023-02-12 09:41:22 +0300

    try

--- src/main/java/com/example/helloworldspringwithdevops/HelloWorldSpringWithDevopsApplication.java
+++ src/main/java/com/example/helloworldspringwithdevops/HelloWorldSpringWithDevopsApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 
-public class HelloWorldApplication {
+public class HelloWorldSpringWithDevopsApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
